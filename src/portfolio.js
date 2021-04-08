b15830f9d616ee90369e0418bdfc58308a491143
Madhura-b <m.basavaraju@ufl.edu>
2021-04-07 22:49:01 -0400

    minor changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -344,7 +344,7 @@ const talkSection = {
 
 const podcastSection = {
   title: emoji("Podcast 🎙️"),
-  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+  subtitle: "Hi podcasting",
 
   // Please Provide with Your Podcast embeded Link
   podcast: [
