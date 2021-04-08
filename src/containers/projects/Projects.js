fa01c5bcd82c29032b1e4b16bdf34aaef1d88f6d
Madhura-b <m.basavaraju@ufl.edu>
2021-04-08 01:27:47 -0400

    Commented out open source

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect, useContext, Suspense, lazy} from "react";
 import "./Project.css";
 import Button from "../../components/button/Button";
-import {openSource, socialMediaLinks} from "../../portfolio";
+import {socialMediaLinks} from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
 import Loading from "../../containers/loading/Loading";
 export default function Projects() {
@@ -40,31 +40,31 @@ export default function Projects() {
   function setrepoFunction(array) {
     setrepo(array);
   }
-  if (
-    !(typeof repo === "string" || repo instanceof String) &&
-    openSource.display
-  ) {
-    return (
-      <Suspense fallback={renderLoader()}>
-        <div className="main" id="opensource">
-          <h1 className="project-title">Open Source Projects</h1>
-          <div className="repo-cards-div-main">
-            {repo.map((v, i) => {
-              return (
-                <GithubRepoCard repo={v} key={v.node.id} isDark={isDark} />
-              );
-            })}
-          </div>
-          <Button
-            text={"More Projects"}
-            className="project-button"
-            href={socialMediaLinks.github}
-            newTab={true}
-          />
-        </div>
-      </Suspense>
-    );
-  } else {
+  // if (
+  //   !(typeof repo === "string" || repo instanceof String) &&
+  //   openSource.display
+  // ) {
+  //   return (
+  //     <Suspense fallback={renderLoader()}>
+  //       <div className="main" id="opensource">
+  //         <h1 className="project-title">Open Source Projects</h1>
+  //         <div className="repo-cards-div-main">
+  //           {repo.map((v, i) => {
+  //             return (
+  //               <GithubRepoCard repo={v} key={v.node.id} isDark={isDark} />
+  //             );
+  //           })}
+  //         </div>
+  //         <Button
+  //           text={"More Projects"}
+  //           className="project-button"
+  //           href={socialMediaLinks.github}
+  //           newTab={true}
+  //         />
+  //       </div>
+  //     </Suspense>
+  //   );
+  // } else {
     return <FailedLoading />;
-  }
+  // }
 }
